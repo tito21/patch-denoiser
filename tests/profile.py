8cef8557e4b84b0320f9c50ba783d89d3f7f4a91
Alberto Di Biase <adibiase@ic.ac.uk>
2025-05-13 19:34:40 +0100

    profile test

---
+++ tests/profile.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+from tests.phantom import phantom_multicontrast
+from patch_denoise import locally_low_rank_tucker
+
+data = phantom_multicontrast(32)
+noise_level = 0.1
+data_noisy = data + noise_level * (np.random.randn(*data.shape) + 1j * np.random.randn(*data.shape))
+
+locally_low_rank_tucker(data_noisy, 3, tau=0.5/noise_level, patch_transform="wavelet", lambda2d=0.1)
